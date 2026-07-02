ec1f87c707a5f6ef5ce967ee100193ed7d62482b
Daniel Wee <danielweedk@gmail.com>
2026-07-02 23:04:34 +0800

    Added note

--- vm/vm.go
+++ vm/vm.go
@@ -2,3 +2,4 @@ package vm
 
 //TODO: https://craftinginterpreters.com/chunks-of-bytecode.html
 //only after making functional lang
+//maybe make in diff prog lang e.g. C++ or rust (and pray genericuser or myself wont pwn its arse)
